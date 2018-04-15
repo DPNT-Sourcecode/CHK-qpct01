f40832fe4b0ea43e369179881c9c722494b9d654
sbx_user1059 <sbx_user1059@ip-10-11-79-235.eu-west-2.compute.internal>
2018-04-15 14:07:41 +0000

    Sun Apr 15 14:07:41 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -4,14 +4,9 @@
 # skus = unicode string
 def checkout(skus):
     skus = ''.join(sorted(skus))
-    discounts = {'AAA': 130, 'BB': 45}
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    prices = (('AAA', 130), ('BB', 45), ('A', 50), ('B', 30), ('C', 20), ('D', 15))
 
-    price, skus = calculate_price(skus, discounts)
-    both = calculate_price(skus, prices)
-
-    price += both[0]
-    skus = both[1]
+    price, skus = calculate_price(skus, prices)
 
     if skus:
         return -1
@@ -20,8 +15,8 @@ def checkout(skus):
 def calculate_price(skus, prices):
     price = 0
 
-    for key in prices.iterkeys():
-        price += skus.count(key) * prices[key]
+    for key, value in prices:
+        price += skus.count(key) * value
         skus = skus.replace(key, '')
     
     return price, skus
