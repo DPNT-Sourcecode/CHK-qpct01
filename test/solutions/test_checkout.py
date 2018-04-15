40a4d29bfca8b4e760edc09fa8aacfa6be5c8233
sbx_user1059 <sbx_user1059@ip-10-11-79-235.eu-west-2.compute.internal>
2018-04-15 13:40:18 +0000

    Sun Apr 15 13:40:18 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -5,43 +5,43 @@ from lib.solutions.checkout import checkout
 
 class TestCheckout(unittest.TestCase):
     def test_invalid(self):
-        self.assertEqual('E', -1)
+        self.assertEqual(checkout('E'), -1)
 
     def test_empty(self):
-        self.assertEqual('', 0)
+        self.assertEqual(checkout(''), 0)
 
     def test_A(self):
-        self.assertEqual('A', 50)
+        self.assertEqual(checkout('A'), 50)
     
     def test_AA(self):
-        self.assertEqual('A', 100)
+        self.assertEqual(checkout('AA'), 100)
     
     def test_AAA(self):
-        self.assertEqual('A', 130)
+        self.assertEqual(checkout('AAA'), 130)
 
     def test_AAAA(self):
-        self.assertEqual('A', 180)
+        self.assertEqual(checkout('AAAA'), 180)
     
     def test_B(self):
-        self.assertEqual('B', 30)
+        self.assertEqual(checkout('B'), 30)
     
     def test_BB(self):
-        self.assertEqual('BB', 45)
+        self.assertEqual(checkout('BB'), 45)
 
     def test_BBB(self):
-        self.assertEqual('BBB', 75)
+        self.assertEqual(checkout('BBB'), 75)
     
     def test_C(self):
-        self.assertEqual('C', 20)
+        self.assertEqual(checkout('C'), 20)
     
     def test_CC(self):
-        self.assertEqual('CC', 40)
+        self.assertEqual(checkout('CC'), 40)
 
     def test_D(self):
-        self.assertEqual('D', 15)
+        self.assertEqual(checkout('D'), 15)
 
     def test_DD(self):
-        self.assertEqual('DD', 30)
+        self.assertEqual(checkout('DD'), 30)
     
 if __name__ == '__main__':
     unittest.main()
