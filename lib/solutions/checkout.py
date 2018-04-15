94a5748ade90d9c0695c15605884d0292824d871
sbx_user1059 <sbx_user1059@ip-10-11-79-235.eu-west-2.compute.internal>
2018-04-15 14:51:21 +0000

    Sun Apr 15 14:51:21 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -5,8 +5,8 @@
 def checkout(skus):
     skus = ''.join(sorted(skus))
 
-    prices = (('AAAAA', 200), ('AAA', 130), ('BB', 45), ('HHHHHHHHHH', 80), ('HHHHH', 45), ('KK', 150), ('PPPPP', 200), ('QQQ', 80), ('VVV', 130), ('VV', 90), ('A', 50), ('B', 30), ('C', 20), ('D', 15), ('E', 40), ('F', 10), ('G', 20), ('H', 10), ('I', 35), ('J', 60), ('K', 80),
-              ('L', 90), ('M', 15), ('N', 40), ('O', 10), ('P', 50), ('Q', 30), ('R', 50), ('S', 30), ('T', 20), ('U', 40), ('V', 50), ('W', 20), ('X', 90), ('Y', 10), ('Z', 50))
+    prices = (('AAAAA', 200), ('AAA', 130), ('BB', 45), ('HHHHHHHHHH', 80), ('HHHHH', 45), ('KK', 120), ('PPPPP', 200), ('QQQ', 80), ('VVV', 130), ('VV', 90), ('A', 50), ('B', 30), ('C', 20), ('D', 15), ('E', 40), ('F', 10), ('G', 20), ('H', 10), ('I', 35), ('J', 60), ('K', 70),
+              ('L', 90), ('M', 15), ('N', 40), ('O', 10), ('P', 50), ('Q', 30), ('R', 50), ('S', 20), ('T', 20), ('U', 40), ('V', 50), ('W', 20), ('X', 17), ('Y', 20), ('Z', 21))
 
     price, skus = buy_n_get_one_for_free(2, skus, 20, 'F')
 
@@ -56,3 +56,6 @@ def buy_n_get_one_for_free(n, skus, price, product):
     group = product * (n + 1)
     count = skus.count(group)
     return count * price, skus.replace(group, '')
+
+def buy_any_three_for_price(skus, price, products):
+    
