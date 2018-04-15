43eb5993fb57590a4091ae8303cbaf44c8c56e7a
sbx_user1059 <sbx_user1059@ip-10-11-79-235.eu-west-2.compute.internal>
2018-04-15 13:43:18 +0000

    Sun Apr 15 13:43:18 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,4 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    return 0
+    discounts = {'AAA': 130, 'BB': 45}
+    prices = { 'A': 50, 'B': 30, 'C': 20, 'D': 15}
+
+    price = 0
+    count_aaa = skus.count('AAA')
+
+    return count_aaa
